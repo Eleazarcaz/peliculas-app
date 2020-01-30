146f4d7ecb3f2cbcd5a356c58fb1fda93ecd55fa
Eleazar <eleazarcaz@outlook.com>
2020-01-30 11:29:51 -0600

    Resolviendo problema de Generos

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -8,16 +8,43 @@ import { map } from 'rxjs/operators';
 export class PeliculasService {
   private api_key: string = '11ddc86886532d7864a6b207f3084193';
   private url: string = 'https://api.themoviedb.org/3';
+  public genres: Array<object>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.getGenres().subscribe(resp => (this.genres = resp));
+  }
 
   getTrendingMovies() {
     return this.http
       .get(`${this.url}/trending/all/week?api_key=${this.api_key}&language=es`)
       .pipe(
         map(resp => {
-          return resp['results'];
+          let peliculas = resp['results'];
+
+          peliculas.forEach(pelicula => {
+            pelicula['generos'] = [];
+            // Ciclo para checar cada id en los generos de la pelicula
+            pelicula['genre_ids'].forEach((genero_id: any) => {
+              let findGenero = this.genres.find(findId);
+
+              if (findGenero !== undefined) {
+                (<Array<any>>pelicula['generos']).push(findGenero);
+              }
+              function findId(obj) {
+                return obj.id === genero_id;
+              }
+            });
+          });
+          return peliculas;
         })
       );
+  }
+
+  getGenres() {
+    return this.http
+      .get(
+        `${this.url}/genre/movie/list?api_key=${this.api_key}&language=es-MX`
+      )
+      .pipe(map(resp => resp['genres']));
   }
 }
